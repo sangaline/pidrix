53dbabb9a6d146eb3a454e250fda7c9151f87584
Evan Sangaline <esangaline@gmail.com>
2014-03-26 15:20:17 -0700

    Added implementations for Euclidian norms.

--- src/norms.cxx
+++ src/norms.cxx
@@ -1,5 +1,35 @@
 #include "norms.h"
-
 #include "Pidrix.h"
 
+#include "TVectorD.h"
+#include "TMatrixD.h"
+#include "TH1.h"
+
+#include "math.h"
+
 using namespace Norms;
+
+double Norms::Euclidian(Pidrix *P) {
+    const TMatrixD A = P->GetU()*P->GetV();
+    return Euclidian(&P->GetT(), &A);
+}
+
+double Norms::Euclidian(const TMatrixD* A, const TMatrixD* B) {
+    return sqrt(((*A)-(*B)).E2Norm());
+}
+
+double Norms::Euclidian(const TVectorD* A, const TVectorD* B) {
+    return sqrt(((*A)-(*B)).Norm2Sqr());
+}
+
+double Norms::Euclidian(const TH1* A, const TH1* B) {
+    const unsigned int xbins = A->GetNbinsX();
+    const unsigned int ybins = A->GetNbinsY();
+    double sum2 = 0;
+    for(unsigned int i = 1; i <= xbins; i++) {
+        for(unsigned int j = 1; j <= ybins; j++) {
+            sum2 += pow(A->GetBinContent(i,j) - B->GetBinContent(i,j), 2);
+        }
+    }
+    return sqrt( sum2/double(xbins*ybins) );
+}
--- src/norms.h
+++ src/norms.h
@@ -1,9 +1,16 @@
 #ifndef norms_h
 #define norms_h
 
+#include "TMatrixDfwd.h"
+#include "TVectorDfwd.h"
 class Pidrix;
+class TH1;
 
 namespace Norms {
+    double Euclidian(Pidrix *P);
+    double Euclidian(const TMatrixD* A, const TMatrixD* B);
+    double Euclidian(const TVectorD* A, const TVectorD* B);
+    double Euclidian(const TH1* A, const TH1* B);
 
 };
 
